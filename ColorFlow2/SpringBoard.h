430879853b9e3b0baa9de95ffeb01767fa9ad829
DavidGoldman <dallasftball@gmail.com>
2015-09-13 17:57:37 -0400

    More ColorFlow 2 SB API

--- ColorFlow2/SpringBoard.h
+++ ColorFlow2/SpringBoard.h
@@ -1,11 +1,22 @@
 // ColorFlow 2 Public SpringBoard API Headers.
 
+// Analyzed Info struct. Having a struct and Obj-C classes is a bit redudant, but I have no plans
+// to change this at the moment. Sorry.
+struct AnalyzedInfo {
+  int bg;
+  int primary;
+  int secondary;
+  BOOL darkBG;
+};
+
 // Contains color info about an analysis.
 @interface CFWAnalyzedInfo : NSObject
 @property(nonatomic, assign) int bgColor;
 @property(nonatomic, assign) int primaryColor;
 @property(nonatomic, assign) int secondaryColor;
 @property(nonatomic, assign, getter=isDarkBG) BOOL darkBG;
+
+- (struct AnalyzedInfo)analyzedInfo;
 @end
 
 // Your class should implement these methods for use as a CFWSBMediaController delegate.
@@ -37,4 +48,24 @@
 
 // Remove your delegate whenever you don't want analysis to occur - i.e. if your view is hidden.
 - (void)removeAnalysisDelegate:(id<CFWAnalysisDelegate>)delegate;
+@end
+
+// Contains color info in object form.
+@interface CFWColorInfo : NSObject
+@property(nonatomic, retain) UIColor *backgroundColor;
+@property(nonatomic, retain) UIColor *primaryColor;
+@property(nonatomic, retain) UIColor *secondaryColor;
+@property(nonatomic, assign, getter=isBackgroundDark) BOOL backgroundDark;
+
++ (instancetype)colorInfoWithAnalyzedInfo:(struct AnalyzedInfo)info;
+
+- (instancetype)initWithAnalyzedInfo:(struct AnalyzedInfo)info;
+@end
+
+// Allows you to easily colorize media controls.
+@interface MPUSystemMediaControlsView : UIView
+@end
+@interface MPUSystemMediaControlsView (ColorFlow2)
+- (void)cfw_colorize:(CFWColorInfo *)colorInfo;
+- (void)cfw_revert;
 @end
